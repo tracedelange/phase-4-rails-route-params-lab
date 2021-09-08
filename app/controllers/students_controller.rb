96a798ef8e1122d64ad37be5aa67b43590871202
Trace <tracedelange@me.com>
2021-09-08 12:10:30 -0500

    commit tree 44adaf489e3463c7e4d95a0778a74d8eef65dac2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,23 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
-    render json: students
+    if params.key?("name")
+      
+      students = Student.where(last_name: params[:name].capitalize).or(Student.where(first_name: params[:name].capitalize))
+      render json: students
+    else
+      render json: students
+    end
+
+
   end
+
+  def find_by_id
+    
+    student = Student.find(params[:id])
+    # byebug
+    render json: student
+  end
+
 
 end
